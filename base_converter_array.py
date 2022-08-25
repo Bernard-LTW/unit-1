62d688ce899186b02517774491c96f57d24a4b74
LeTsWo <34589202+Bernard-LTW@users.noreply.github.com>
2022-08-25 21:35:06 +0900

    Create base_converter_array.py
    2022-08-25 - Base Converter by Bernard Lee
    This program converts a positive integer from Base 10 to Base 2-9 inclusive with an array.

---
+++ base_converter_array.py
@@ -0,0 +1,30 @@
+#2022-08-25 - Base Converter by Bernard Lee
+#This program converts a positive integer from Base 10 to Base 2-9 inclusive with an array.
+
+#Initizalize variables
+orig_num = int(input("Input the positive number you want to convert: "))
+base = int(input("Input the base you want to convert to from 2 to 9: "))
+output_storage = []
+
+#Check for invalid base
+if base < 3 or base > 10:
+    print("Base must be from 2 to 9 inclusive")
+    exit()
+
+#Main calculation
+div = int(orig_num/base)
+mod = orig_num % base
+output_storage.append(mod)
+
+while div > base-1:
+    mod = int(div%base)
+    div = int(div/base)
+    output_storage.append(mod)
+
+output_storage.append(div)
+
+#Print output
+output_storage.reverse()
+print(output_storage)
+
+##End of program##
